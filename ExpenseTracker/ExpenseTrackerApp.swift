1cd9b329b1aa84ae538745824fe5452176d41dca
MichaelRomeroSG <michael@socialgoodsoftware.com>
2024-05-14 18:56:32 -0500

    Initial Commit

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Michael Romero on 14/05/24.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

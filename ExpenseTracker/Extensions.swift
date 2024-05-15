8e4123dc747b1cef70ad79a185c95f10a28d3c01
MichaelRomeroSG <michael@socialgoodsoftware.com>
2024-05-14 20:06:58 -0500

    Init repository

---
+++ ExpenseTracker/Extensions.swift
@@ -0,0 +1,16 @@
+//
+//  Extensions.swift
+//  ExpenseTracker
+//
+//  Created by Michael Romero on 14/05/24.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color {
+    static let customBackground = Color("Background")
+    static let customIcon = Color("Icon")
+    static let customText =  Color("Text")
+    static let customSystemBackground =  Color(uiColor: .systemBackground)
+}
